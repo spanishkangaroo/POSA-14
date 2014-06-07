c28c868b6fd18db4e8a5dc1832544735bf0e296c
spanishkangaroo <javier.vazquez.romera@gmail.com>
2014-06-07 19:02:32 +0200

    W3-A2 TODOs completed and JUnit tests ran successfully

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,70 +1,119 @@
 package edu.vuum.mocca;
 
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer.ConditionObject;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
- * 
+ *
  * @brief This class provides a simple counting semaphore
  *        implementation using Java a ReentrantLock and a
- *        ConditionObject (which is accessed via a Condition). It must
- *        implement both "Fair" and "NonFair" semaphore semantics,
- *        just liked Java Semaphores.
+ *        ConditionObject.  It must implement both "Fair" and
+ *        "NonFair" semaphore semantics, just liked Java Semaphores. 
  */
 public class SimpleSemaphore {
     /**
-     * Define a ReentrantLock to protect the critical section.
+     * Constructor initialize the data members.  
      */
-    // TODO - you fill in here
-
-    /**
-     * Define a Condition that waits while the number of permits is 0.
-     */
-    // TODO - you fill in here
-
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here.  Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
-
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
+    public SimpleSemaphore (int permits,
+                            boolean fair)
+    { 
+        // TODO - you fill in here
+    	mRWLock = new ReentrantLock(fair);
+    	permitted = mRWLock.newCondition();
+    	mRWLock.lock(); 
+    	try { 
+    		availablePermits = permits; 
+    	} finally { 
+    		mRWLock.unlock(); 
+    	} 
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that can
+     * be interrupted.
      */
     public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+		mRWLock.lockInterruptibly();
+
+    	try {
+    		 while (availablePermits() <= 0) {
+                 permitted.await();
+             }
+    		 --availablePermits;
+    	} finally {
+    		mRWLock.unlock();
+    	}
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that
+     * cannot be interrupted.
      */
     public void acquireUninterruptibly() {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+    	mRWLock.lock();
+
+    	try {
+    		 while (availablePermits() <= 0) {
+                 permitted.await();
+             }
+    		 --availablePermits;
+    	} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+    		mRWLock.unlock();
+    	}
     }
 
     /**
      * Return one permit to the semaphore.
      */
     void release() {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+    	mRWLock.lock(); 
+    	try { 
+    		++availablePermits; 
+    		permitted.signal(); 
+    	} finally { 
+    		mRWLock.unlock(); 
+    	} 
     }
-
+    
     /**
      * Return the number of permits available.
      */
-    public int availablePermits() {
-        // TODO - you fill in here by changing null to the appropriate
-        // return value.
-        return null;
+    public int availablePermits(){
+    	// TODO - you fill in here
+    	mRWLock.lock(); 
+    	try { 
+    		return availablePermits; 
+    	} finally { 
+    		mRWLock.unlock(); 
+    	} 
     }
+    
+    /**
+     * Define a ReentrantLock to protect the critical section.
+     */
+    // TODO - you fill in here
+    private ReentrantLock mRWLock;
+
+    /**
+     * Define a ConditionObject to wait while the number of
+     * permits is 0.
+     */
+    // TODO - you fill in here
+    private Condition permitted;
+
+    /**
+     * Define a count of the number of available permits.
+     */
+    // TODO - you fill in here.  Make sure that this data member will
+    // ensure its values aren't cached by multiple Threads..
+    private int availablePermits;
 }
+
